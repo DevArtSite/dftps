d472a786f7c0982b36e3fcc1d38344d3214a64f8
MNLaugh <contact@mnlaugh.com>
2021-05-16 14:14:20 +0200

    Update dependencies in deps.ts

--- deps.ts
+++ deps.ts
@@ -1,20 +1,20 @@
-export { deferred, MuxAsyncIterator } from "https://deno.land/std@0.95.0/async/mod.ts";
-export type { Deferred } from "https://deno.land/std@0.95.0/async/mod.ts";
-export { DenoStdInternalError } from "https://deno.land/std@0.95.0/_util/assert.ts";
-export { BufWriter, BufReader } from "https://deno.land/std@0.95.0/io/bufio.ts";
-export { assert } from "https://deno.land/std@0.95.0/_util/assert.ts";
-export { v4 } from "https://deno.land/std@0.95.0/uuid/mod.ts";
-export * as DPath from "https://deno.land/std@0.95.0/path/mod.ts";
-export { exists } from "https://deno.land/std@0.95.0/fs/mod.ts";
-export { writableStreamFromWriter, readableStreamFromReader } from "https://deno.land/std@0.95.0/io/streams.ts";
+export { deferred, MuxAsyncIterator } from "https://deno.land/std@0.96.0/async/mod.ts";
+export type { Deferred } from "https://deno.land/std@0.96.0/async/mod.ts";
+export { DenoStdInternalError } from "https://deno.land/std@0.96.0/_util/assert.ts";
+export { BufWriter, BufReader } from "https://deno.land/std@0.96.0/io/bufio.ts";
+export { assert } from "https://deno.land/std@0.96.0/_util/assert.ts";
+export { v4 } from "https://deno.land/std@0.96.0/uuid/mod.ts";
+export * as DPath from "https://deno.land/std@0.96.0/path/mod.ts";
+export { exists } from "https://deno.land/std@0.96.0/fs/mod.ts";
+export { writableStreamFromWriter, readableStreamFromReader } from "https://deno.land/std@0.96.0/io/streams.ts";
 export { format } from "https://deno.land/std@0.96.0/datetime/mod.ts";
 
 export { getPort, makeRange, randomPort } from "https://deno.land/x/getport@2.1.0/mod.ts";
 
-export { Command } from "https://deno.land/x/cliffy/command/mod.ts";
+export { Command } from "https://deno.land/x/cliffy@v0.18.2/command/mod.ts";
 export { Table } from "https://deno.land/x/cliffy@v0.18.2/table/mod.ts";
-export { colors } from "https://deno.land/x/cliffy/ansi/colors.ts";
-export { hash, verify } from "https://deno.land/x/scrypt/mod.ts";
+export { colors } from "https://deno.land/x/cliffy@v0.18.2/ansi/colors.ts";
+export { hash, verify } from "https://deno.land/x/scrypt@v2.0.0/mod.ts";
 
 export {
   Database,
@@ -24,5 +24,5 @@ export {
   SQLite3Connector,
   Model,
   DataTypes
-} from 'https://deno.land/x/denodb/mod.ts';
-export type { MongoDBOptions, MySQLOptions, PostgresOptions, SQLite3Options } from 'https://deno.land/x/denodb/mod.ts';
+} from 'https://deno.land/x/denodb@v1.0.38/mod.ts';
+export type { MongoDBOptions, MySQLOptions, PostgresOptions, SQLite3Options } from 'https://deno.land/x/denodb@v1.0.38/mod.ts';
