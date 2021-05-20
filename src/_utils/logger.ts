efbbd26588966700175653584c1ccb92d12601c5
MNLaugh <contact@mnlaugh.com>
2021-05-20 15:33:13 +0200

    fix options setting in src/_utils/logger.ts

--- src/_utils/logger.ts
+++ src/_utils/logger.ts
@@ -1,4 +1,3 @@
-import { v4 } from "https://deno.land/std@0.76.0/uuid/mod.ts";
 export enum Colors {
   Reset = "\x1b[0m",
   Bright = "\x1b[1m",
@@ -46,105 +45,79 @@ export enum Colors {
   BgLiteWhite = "\x1b[107m",
 }
 
-interface LoggerOptions {
+export interface LoggerOptions {
   prefix?: string | null;
   colorize?: boolean;
   preLine?: string;
   color?: string;
 }
 
-const defaultOptions = {
-  prefix: null,
-  colorize: true,
-  preLine: '  '
-}
-
-export default class Logger {
-  options: LoggerOptions;
+class Logger {
+  private options: LoggerOptions;
   constructor (options: LoggerOptions = {}) {
-    if (!options) options = defaultOptions;
-    this.options = Object.assign(defaultOptions, options)
+    if (options.colorize === undefined) options.colorize = true;
+    if (options.preLine === undefined) options.preLine = "  ";
+    this.options = options;
   }
 
-  create(options: LoggerOptions): Logger {
-    return new Logger(options)
+  static create(options: LoggerOptions): Logger {
+    return new Logger(options);
   }
 
   // deno-lint-ignore no-explicit-any
   log(...args: any): void {
-    const options = getOptions.apply(this, [Colors.FgWhite])
-    handle(options, ...args);
+    const options = Object.assign(this.options, { color: Colors.FgWhite });
+    this.handle(options, ...args);
   }
 
   // deno-lint-ignore no-explicit-any
   success(...args: any): void {
-    const options = getOptions.apply(this, [Colors.FgLiteGreen])
-    handle(options, ...args);
+    const options = Object.assign(this.options, { color: Colors.FgLiteGreen });
+    this.handle(options, ...args);
   }
 
   // deno-lint-ignore no-explicit-any
   info(...args: any): void {
-    const options = getOptions.apply(this, [Colors.FgLiteBlue])
-    handle(options, ...args);
+    const options = Object.assign(this.options, { color: Colors.FgLiteBlue });
+    this.handle(options, ...args);
   }
 
   // deno-lint-ignore no-explicit-any
   warn(...args: any): void {
-    const options = getOptions.apply(this, [Colors.FgLiteYellow])
-    handle(options, ...args);
+    const options = Object.assign(this.options, { color: Colors.FgLiteYellow });
+    this.handle(options, ...args);
   }
 
   // deno-lint-ignore no-explicit-any
   error(...args: any): void {
-    const options = getOptions.apply(this, [Colors.FgLiteRed])
-    args = format(options, ...args);
-    handle(options, Colors.Reset, ...args);
+    const options = Object.assign(this.options, { color: Colors.FgLiteRed });
+    this.handle(options, ...args);
   }
 
   // deno-lint-ignore no-explicit-any
   debug(...args: any): void {
-    const options = getOptions.apply(this, [Colors.FgLiteMagenta])
-    args = format(options, ...args);
-    handle(options, Colors.Reset, ...args);
+    const options = Object.assign(this.options, { color: Colors.FgLiteMagenta });
+    this.handle(options, ...args);
+  }
+
+  // deno-lint-ignore no-explicit-any
+  private handle(options: LoggerOptions, ...args: any): void {
+    console.log(
+      (this.options.colorize) ? Colors.FgLiteCyan : "",
+      `[${new Date().toISOString()}]`,
+      (this.options.colorize) ? options.color : "",
+      options.prefix,
+      // deno-lint-ignore no-explicit-any
+      args.map((arg: any) => {
+        return (typeof arg === "string" || typeof args === "number") ? arg
+        : (arg instanceof Error) ? `${arg.stack}`
+          : (arg instanceof Array) ? arg.toString()
+            : (typeof arg === "object") ? JSON.stringify(arg)
+              : arg;
+      }).join(" "),
+      (this.options.colorize) ? Colors.Reset : ""
+    );
   }
 }
 
-function getOptions (this: Logger, color: string): LoggerOptions {
-    let options;
-    if (!(this instanceof Logger)) options = Object.assign(defaultOptions, { color });
-    else options = Object.assign(this.options, { color });
-    return options
-}
-
-function applyColor(color: string, message: string): string {
-  return color + message + Colors.Reset;
-}
-
-// deno-lint-ignore no-explicit-any
-function handle(options: LoggerOptions, ...args: any): void {
-  console.log(
-    options.color,
-    options.prefix,
-    ...args,
-    Colors.Reset
-  )
-}
-
-// deno-lint-ignore no-explicit-any
-function format(options: LoggerOptions, ...args: any): any[]{
-  // deno-lint-ignore no-explicit-any
-  const data: Array<any> = []
-  data.push('{\n');
-  // deno-lint-ignore no-explicit-any
-  args.forEach((arg: any): void => {
-    data.push(applyColor(options.color || Colors.FgLiteWhite, options.preLine || defaultOptions.preLine));
-    if (typeof arg === 'string') data.push(options.color + arg + Colors.Reset)
-    else if (arg instanceof Error) {
-      if (arg.message) arg.message = applyColor(options.color || Colors.FgLiteWhite, arg.message);
-      data.push(arg);
-    } else data.push(arg);
-    data.push('\n');
-  })
-  data.push('}\n');
-  return data;
-}
+export default Logger;
