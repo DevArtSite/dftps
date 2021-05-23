5b5047a38108317a77d4471a9db976909c75ab34
MNLaugh <contact@mnlaugh.com>
2021-05-24 01:01:29 +0200

    missing version

---
+++ tools/commits.ts
@@ -0,0 +1,17 @@
+const res: Response | void = await fetch("https://api.github.com/repos/devartsite/dftps/commits").catch(console.error);
+if (!res) throw new Error("coucou");
+const commits = await res.json();
+
+const newCommits = [];
+const lastCommitID = "d4803bd";
+for (const commit of commits) {
+  if (commit.sha.search(lastCommitID) !== -1) {
+    console.log("ici")
+    break;
+  }
+  else newCommits.push(commit);
+}
+
+newCommits.forEach(({ sha, commit, html_url }) => {
+  console.log(`${commit.message} ([${sha.substring(0, 7)}](${html_url}))`)
+})
---
+++ tools/create.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if ! command -v zip >/dev/null; then
+	echo "Error: zip is required." 1>&2
+	exit 1
+fi
+
+archive="dftps.zip"
+
+sudo ~/.deno/bin/deno compile -A --unstable ../dftps.ts
+
+if [ -f "$archive" ]; then
+  rm "$archive"
+fi
+
+cp default.config.toml dftps.toml
+
+zip "$archive" dftps dftps.toml
+
+rm dftps
+rm dftps.toml
